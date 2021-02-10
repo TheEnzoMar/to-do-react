1d383b463cc4d4c3779e6237f6f50b5139919f00
Enzo Marchioni <enzo.marchioni@shopify.com>
2021-02-10 11:29:13 -0500

    Refactored out tests to use new renderHook helper - Added @testing-library

--- src/hooks/useTodosState.test.tsx
+++ src/hooks/useTodosState.test.tsx
@@ -1,32 +1,18 @@
 import React from 'react';
-import { mount } from '@shopify/react-testing';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useTodosState } from './useTodosState';
 import { createTodo } from '../factory';
 import { Todo } from '../types';
 
-const InnerComponent = (props: any) => <></>;
-
-interface Props {
-  initialState?: Todo[];
-}
-const TestComponent = ({ initialState }: Props) => {
-  const hookValues = useTodosState(initialState || []);
-
-  return <InnerComponent {...hookValues} />;
-};
-
 describe('useTodosState', () => {
   describe('newTodo', () => {
     it('create new Todo item in state', () => {
-      const wrapper = mount(<TestComponent />);
-
-      wrapper?.act(async () => {
-        const innner = wrapper.find(InnerComponent);
-        await innner?.props.newTodo(createTodo({ title: 'Sample Todo' }));
+      const { result } = renderHook(useTodosState);
+      act(() => {
+        result.current.newTodo(createTodo({ title: 'Sample Todo' }));
       });
 
-      const inner = wrapper.find(InnerComponent);
-      expect(inner?.props.todos).toHaveLength(1);
+      expect(result.current.todos).toHaveLength(1);
     });
   });
 
@@ -37,22 +23,24 @@ describe('useTodosState', () => {
         createTodo({ id: '2', title: 'Todo 2' }),
       ];
 
-      const wrapper = mount(<TestComponent initialState={initialState} />);
-
-      wrapper?.act(async () => {
-        const inner = wrapper.find(InnerComponent);
-        await inner?.props.updateTodo({ id: '2', title: 'Todo 2 updated' });
+      const { result } = renderHook(useTodosState, {
+        initialProps: initialState,
       });
 
-      const inner = wrapper.find(InnerComponent);
-      expect(inner?.props.todos[1].title).toBe('Todo 2 updated');
+      act(() => {
+        result.current.updateTodo({
+          id: '2',
+          title: 'Todo 2 Updated',
+          completed: false,
+        });
+      });
+
+      expect(result.current.todos[1].title).toBe('Todo 2 Updated');
     });
 
     it('rejects promise if Todo item is not found in state', async () => {
-      const wrapper = mount(<TestComponent />);
-      const inner = wrapper.find(InnerComponent);
-
-      await expect(inner?.props.updateTodo(createTodo())).rejects.toEqual({
+      const { result } = renderHook(useTodosState);
+      await expect(result.current.updateTodo(createTodo())).rejects.toEqual({
         error: 'No Todo to update was found',
         success: false,
       });
@@ -67,22 +55,20 @@ describe('useTodosState', () => {
         createTodo({ id: '3', title: 'Todo 3' }),
       ];
 
-      const wrapper = mount(<TestComponent initialState={initialState} />);
-
-      wrapper?.act(async () => {
-        const innner = wrapper.find(InnerComponent);
-        await innner?.props.deleteTodo('2');
+      const { result } = renderHook(useTodosState, {
+        initialProps: initialState,
       });
 
-      const inner = wrapper.find(InnerComponent);
-      expect(inner?.props.todos).toHaveLength(2);
+      act(() => {
+        result.current.deleteTodo('2');
+      });
+
+      expect(result.current.todos).toHaveLength(2);
     });
 
     it('rejects the promise if id param is undefined', async () => {
-      const wrapper = mount(<TestComponent />);
-      const innner = wrapper.find(InnerComponent);
-
-      await expect(innner?.props.deleteTodo(undefined)).rejects.toEqual({
+      const { result } = renderHook(useTodosState);
+      await expect(result.current.deleteTodo(undefined)).rejects.toEqual({
         error: 'No ID provide',
         success: false,
       });
